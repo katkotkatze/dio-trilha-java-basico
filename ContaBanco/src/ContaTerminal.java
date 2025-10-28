6c261302e45da5b7b063c367db1e6b4ce665f931
Kat <238366106+katkotkatze@users.noreply.github.com>
2025-10-28 19:27:08 -0300

    commit código

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        criarConta();
+
+    }
+
+    static void criarConta() {
+        var scannerConta = new Scanner(System.in);
+        System.out.println("Por favor,digite o seu Nome! (Exemplo: Mario Andrade)");
+        var nomeCliente = scannerConta.nextLine();
+        System.out.println("Por favor,digite o número da Agência! (Exemplo: 067-8)");
+        var numeroAgencia = scannerConta.nextLine();
+        System.out.println("Por favor,digite o número da Conta! (Exemplo: 1021)");
+        var numeroConta = scannerConta.nextInt();
+
+        double saldoConta = 237.48;
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco" +
+                ", sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque\n", nomeCliente,
+                numeroAgencia,
+                numeroConta, saldoConta);
+
+        scannerConta.close();
+
     }
 }
